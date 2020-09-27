bab8b5b190995fb662bb8882520c799049f6b9d1
Abu <abudarda166@gmail.com>
2020-09-27 17:43:39 +0100

    Add author model
    * create unit test for author
    * create author table
    * apply migrations
    * add rest framework in settings

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """Model for an author"""
+    author_id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,3 +1,19 @@
-# from django.test import TestCase
+from django.test import TestCase
+from core import models
 
-# Create your tests here.
+
+class ModelTests(TestCase):
+
+    def test_create_author(self):
+        """
+        Test for succesfully create author with name
+        """
+
+        author = models.Author.objects.create(
+            name='lorem ipsum'
+        )
+
+        self.assertEqual(str(author), author.name)
+        self.assertTrue(
+            isinstance(author.author_id, int)
+        )
