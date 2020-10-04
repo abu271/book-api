aaf5e3ae79b062c15dede2965c8236ba37b32e8f
Abu <abudarda166@gmail.com>
2020-10-04 21:05:17 +0100

    Add model for books
    * create unit test for books with one or more authors
    * makemigrations and migrate for book model

--- app/core/models.py
+++ app/core/models.py
@@ -8,3 +8,15 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    """Model for a book"""
+    book_id = models.AutoField(primary_key=True, editable=False)
+    name = models.CharField(max_length=255)
+    edition = models.CharField(max_length=50)
+    publication_year = models.IntegerField()
+    authors = models.ManyToManyField(Author)
+
+    def __str__(self):
+        return self.name
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,10 +10,72 @@ class ModelTests(TestCase):
         """
 
         author = models.Author.objects.create(
-            name='lorem ipsum'
+            name='Joe Bloggs'
         )
 
         self.assertEqual(str(author), author.name)
         self.assertTrue(
             isinstance(author.author_id, int)
+        )
+
+    def test_create_book_with_one_author(self):
+        """
+        Test for creating a book with one author
+        """
+
+        book = models.Book.objects.create(
+            name='Recipe Book',
+            edition='2nd',
+            publication_year=2005
+        )
+        book.save()
+
+        author = book.authors.create(name='Jane Bloggs')
+        author.save()
+
+        self.assertEqual(book.authors.all()[0], author)
+        self.assertEqual(author.name, 'Jane Bloggs')
+
+        self.assertEqual(str(book), book.name)
+        self.assertEqual(book.edition, '2nd')
+        self.assertEqual(book.publication_year, 2005)
+        self.assertTrue(
+            isinstance(book.book_id, int)
+        )
+
+    def test_create_book_with_multiple_authors(self):
+        """
+        Test for creating a book with three authors
+        """
+
+        book = models.Book.objects.create(
+            name='Photography Book',
+            edition='1st',
+            publication_year=2010
+        )
+        book.save()
+
+        author1 = book.authors.create(name='Jane Bloggs')
+        author1.save()
+
+        author2 = book.authors.create(name='Lorem Ipsum')
+        author2.save()
+
+        author3 = book.authors.create(name='Steve Brick')
+        author3.save()
+
+        self.assertEqual(book.authors.all()[0], author1)
+        self.assertEqual(author1.name, 'Jane Bloggs')
+
+        self.assertEqual(book.authors.all()[1], author2)
+        self.assertEqual(author2.name, 'Lorem Ipsum')
+
+        self.assertEqual(book.authors.all()[2], author3)
+        self.assertEqual(author3.name, 'Steve Brick')
+
+        self.assertEqual(str(book), book.name)
+        self.assertEqual(book.edition, '1st')
+        self.assertEqual(book.publication_year, 2010)
+        self.assertTrue(
+            isinstance(book.book_id, int)
         )
