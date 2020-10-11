78453bfed1eb5465b1b877234b1e9b543b8272a0
Abu <abudarda166@gmail.com>
2020-10-11 22:58:28 +0100

    Add test files
    * add unit test file for custom command
    * add test CSV data (test.csv)

---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,22 @@
+from django.core.management import call_command
+from django.test import TestCase
+from core import models
+
+
+class CommandTests(TestCase):
+
+    def test_import_authors_via_csv_file(self):
+        """
+        Test import_authors command on test.csv file
+        """
+        call_command('import_authors', csv_file='/app/core/tests/test.csv')
+
+        authors = models.Author.objects.all()
+
+        self.assertEqual(len(authors), 5)
+
+        # check first and last author names
+        first_author = str(authors[0])
+        last_author = str(authors[4])
+        self.assertEqual(first_author, 'Edith Perry')
+        self.assertEqual(last_author, 'Paige Moore')
