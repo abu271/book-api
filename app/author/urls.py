b32ad18347ce9b7f78e2e5a520494ef1d4804fbe
Abu <abudarda166@gmail.com>
2020-12-03 21:40:25 +0000

    Edit file configuration for views
    * views are no longer in seperate folders

--- app/author/urls.py
+++ app/author/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import author_views
+from author import views
 
 app_name = 'author'
 
 urlpatterns = [
-    path('author', author_views.AuthorApi.as_view()),
+    path('author', views.AuthorApi.as_view()),
     path(
             'author/create',
-            author_views.AuthorCreateApi.as_view(),
+            views.AuthorCreateApi.as_view(),
             name='create'
         )
 ]
--- app/book/urls.py
+++ app/book/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from .views import book_views
+from book import views
 
 app_name = 'book'
 
 urlpatterns = [
-    path('book', book_views.BookApi.as_view()),
+    path('book', views.BookApi.as_view()),
     path(
             'book/create',
-            book_views.BookCreateApi.as_view(),
+            views.BookCreateApi.as_view(),
             name='create'
         )
 ]
