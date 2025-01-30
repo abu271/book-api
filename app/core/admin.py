7e88b1e0cdf7a4074a7786354dc56cc6466596a5
Abu <abudarda166@gmail.com>
2025-01-30 19:50:18 +0000

    feat: add book & author models to admin site

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+from core import models 
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+  ordering = ['author_id']
+  list_display = [field.name for field in models.Author._meta.fields]
+
+class BookAdmin(admin.ModelAdmin):
+  ordering = ['book_id']
+  list_display = [field.name for field in models.Book._meta.fields]
+
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Book, BookAdmin)
