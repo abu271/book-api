4a30d3b047eaf6e2f3697db094ae80477bdca36f
Abu Darda <38472867+abu271@users.noreply.github.com>
2025-02-08 17:03:44 +0000

    fix: remove custom wait command & use healthcheck in compose(#22)
    *  specify port 
    * remove custom wait command & use healthcheck in compose

--- app/app/settings.py
+++ app/app/settings.py
@@ -111,6 +111,7 @@ else:
             'NAME': os.environ.get('DB_NAME'),
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'PORT': os.environ.get('DB_PORT'),
         }
     }
 
